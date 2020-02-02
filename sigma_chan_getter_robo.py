6f8ed8e75e0a4eab73eb83d5a3a50cf0e484cac3
sakairi-nobuyuki <SAKAIRI.Nobuyuki@gmail.com>
2020-02-03 00:49:26 +0900

    add ascii art

--- sigma_chan_getter_robo.py
+++ sigma_chan_getter_robo.py
@@ -48,6 +48,36 @@ def print_gesudana ():
     print ('XX                                                           XX')
     print ('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
 
+
+def print_tantsubo ():
+    print ('GESUGESUGESUGESUGESUGESUGESUGESUGESUGESU')
+    print ('GE                                    SU')
+    print ('GE         GESUGESUGESUG              SU')
+    print ('GE     GESUG             GESU         SU')
+    print ('GE    SUG    UGE    ESU    UGE        SU')
+    print ('GE     UGESUG          G ESU          SU')
+    print ('GE     UG SUGESUGESUGESUGESU          SU')
+    print ('GE    SU SU   ESUGESUG  UGESU         SU')
+    print ('GE   ESU                  ESUG        SU')
+    print ('GE  GEU  ESUG         SU ES  EU       SU')
+    print ('GE UG SU      SUG SUGE   E   ES       SU')
+    print ('GE UG  UGE             GE    ESU      SU')
+    print ('GE UG    ESUG    E UGE    SU   UG     SU')
+    print ('GE U ES        UGES      ES     G     SU')
+    print ('GE U    GESU         ESU      SUG     SU')
+    print ('GE  G       GESUGES         GES       SU')
+    print ('GESUGESUGESUGESUGESUGESUGESUGESUGESUGESU')
+    print ('GESUGESUGESUGESUGESUGESUGESUGESUGESUGESU')
+                                                            
+
+    
+
+
+
+
+
+
+    
 def get_keyword_to_search ():    
     print ('どんなクソ共のツイートをしりたいのかな？：')
     keyword = input ('$ ')
